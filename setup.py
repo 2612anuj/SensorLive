fee53ec11507440b86ca1436c17c033175d2c0a6
anuj ghogare <anujghogare6@gmail.com>
2025-11-29 11:30:23 +0530

    Initial Project Structure

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages,setup
+
+
+
+setup(
+    name='sensor',
+    version='0.0.1',
+    author='anuj',
+    author_email='anujghogare26@gmail.com',
+    packages=find_packages(),
+    install_requires=['pymongo']
+
+
+)
